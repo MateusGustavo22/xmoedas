6f2af51e9a7d28ec3e60c912bcf9d5f54fbf2136
Mateus <mateus20040914@gmail.com>
2023-04-11 11:26:11 -0300

    codigo do analytics

--- pages/_document.js
+++ pages/_document.js
@@ -24,19 +24,15 @@ export default class MyDocument extends Document {
       <meta name="msapplication-tap-highlight" content="no" />
       <link rel="manifest" href="/manifest.json" />
 
-      {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LWHWX85ZP8"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-LWHWX85ZP8');
-        `,
-        }}
-      />
-     
+      {/*-- Google tag (gtag.js) */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-DS763N9YLS"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+        gtag('config', 'G-DS763N9YLS');
+      </script>
+      
       </Head>
         
         <body>
